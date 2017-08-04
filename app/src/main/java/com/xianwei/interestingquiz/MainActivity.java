450f076f3450a3e35a3a588db5c77c11769af679
xianwei li <xil148@pitt.edu>
2017-08-03 23:25:35 -0400

    finished and tested

--- app/src/main/java/com/xianwei/interestingquiz/MainActivity.java
+++ app/src/main/java/com/xianwei/interestingquiz/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,13 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     OneAnswerQuestion firstOneAnswerQuestion;
+    OneAnswerQuestion secondOneAnswerQuestion;
     MultiAnswersQuestion firstMultiAnswersQuestion;
+    MultiAnswersQuestion secondMultiAnswersQuestion;
+    View questionOneView = null;
+    View questionTwoView = null;
+    View questionThreeView = null;
+    View questionFourView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +41,90 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupUI() {
         createQuestions();
-        View viewOne = createOneAnswerView(firstOneAnswerQuestion);
+        questionOneView = createOneAnswerView(firstOneAnswerQuestion);
+        questionTwoView = createOneAnswerView(secondOneAnswerQuestion);
+        questionThreeView = createMultiAnswersView(firstMultiAnswersQuestion);
+        questionFourView = createMultiAnswersView(secondMultiAnswersQuestion);
 
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.questions_view);
-        linearLayout.addView(viewOne);
+        linearLayout.addView(questionOneView);
+        linearLayout.addView(questionTwoView);
+        linearLayout.addView(questionThreeView);
+        linearLayout.addView(questionFourView);
 
+        Button submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    createToast(submit());
+                }
+            }
+        );
+    }
+
+    private String submit() {
+        int score = 0;
+        RadioButton questionOneAnswerRadio = (RadioButton) questionOneView.findViewById(R.id.radio_button_c);
+        RadioButton questionTwoAnswerRadio = (RadioButton) questionTwoView.findViewById(R.id.radio_button_a);
+
+        CheckBox questionThreeCheckboxOne = (CheckBox) questionThreeView.findViewById(R.id.checkbox_a);
+        CheckBox questionThreeCheckboxTwo = (CheckBox) questionThreeView.findViewById(R.id.checkbox_b);
+        CheckBox questionThreeCheckboxThree = (CheckBox) questionThreeView.findViewById(R.id.checkbox_c);
+        CheckBox questionThreeCheckboxFour = (CheckBox) questionThreeView.findViewById(R.id.checkbox_d);
+
+        CheckBox questionFourCheckboxOne = (CheckBox) questionFourView.findViewById(R.id.checkbox_a);
+        CheckBox questionFourCheckboxTwo = (CheckBox) questionFourView.findViewById(R.id.checkbox_b);
+        CheckBox questionFourCheckboxThree = (CheckBox) questionFourView.findViewById(R.id.checkbox_c);
+        CheckBox questionFourCheckboxFour = (CheckBox) questionFourView.findViewById(R.id.checkbox_d);
+
+        if (questionOneAnswerRadio.isChecked() == true){
+            score += 25;
+        }
+        if (questionTwoAnswerRadio.isChecked() == true) {
+            score += 25;
+        }
+        if (questionThreeCheckboxOne.isChecked() == false &&
+            questionThreeCheckboxTwo.isChecked() == true &&
+            questionThreeCheckboxThree.isChecked() == true &&
+            questionThreeCheckboxFour.isChecked() == true) {
+            score +=25;
+        }
+
+        if (questionFourCheckboxOne.isChecked() == true &&
+            questionFourCheckboxTwo.isChecked() == false &&
+            questionFourCheckboxThree.isChecked() == true &&
+            questionFourCheckboxFour.isChecked() == false) {
+            score +=25;
+        }
+
+        return "Your final score is :" + score + "/100";
     }
 
     private void createQuestions(){
-        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.mountfuji);
-        firstOneAnswerQuestion = new OneAnswerQuestion("Which country does this landmark come from ?",image,"China", "Thailand","Japan","This is Mountain FuJi");
+        Bitmap imageOne = BitmapFactory.decodeResource(getResources(), R.drawable.mountfuji);
+        firstOneAnswerQuestion = new OneAnswerQuestion("Question One:\nWhich country does this landmark belongs to ?",imageOne,"China", "Korea","Japan","This is Mountain FuJi");
+
+        Bitmap imageTwo = BitmapFactory.decodeResource(getResources(), R.drawable.machupicchu);
+        secondOneAnswerQuestion = new OneAnswerQuestion("Question Two:\nWhich country does this landmark belongs to ?",imageTwo,"Peru", "Chile","Brazil","This is Machu Picchu");
+
+        Bitmap imageThree = BitmapFactory.decodeResource(getResources(), R.drawable.tajmahal);
+        firstMultiAnswersQuestion = new MultiAnswersQuestion("Question Three:\nPlease choose the right description of this building.",
+                                                                imageThree,
+                                                                "This building is in Thailand",
+                                                                "This building is in India",
+                                                                "The building is a tomb",
+                                                                "It is one of the New 7 Wonders of the World",
+                                                                "Its name is Taj Mahal");
+
+        Bitmap imageFour = BitmapFactory.decodeResource(getResources(), R.drawable.chichenitza);
+        secondMultiAnswersQuestion = new MultiAnswersQuestion("Question Four:\nPlease choose the right description of this building.",
+                                                                imageFour,
+                                                                "It is one of the New 7 Wonders of the World",
+                                                                "It is in Egypt",
+                                                                "It is in Mexico",
+                                                                "It was built before 2500 BCE",
+                                                                "I can\'t tell you !");
+
     }
 
 
@@ -54,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
         questionView.setText(question.getQuestion());
 
         ImageView imageView = (ImageView) newView.findViewById(R.id.one_answer_image_view);
+        imageView.setScaleType(ImageView.ScaleType.CENTER);
         imageView.setImageBitmap(question.getImage());
 
         RadioButton buttonA = (RadioButton) newView.findViewById(R.id.radio_button_a);
